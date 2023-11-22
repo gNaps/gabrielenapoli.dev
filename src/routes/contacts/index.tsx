80c82dfa91b889469a4739ae418480f2b97b6b59
Gabriele Napoli <gabriele.nap@gmail.com>
2023-11-22 23:08:47 +0100

    reset form contact

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -4,7 +4,7 @@ import {
   type DocumentHead,
   server$,
 } from "@builder.io/qwik-city";
-// import { ContainerAnimated } from "~/components/container-animated/container-animated";
+import { ContainerAnimated } from "~/components/container-animated/container-animated";
 import type { InitialValues, SubmitHandler } from "@modular-forms/qwik";
 import { formAction$, reset, useForm, valiForm$ } from "@modular-forms/qwik";
 import { Resend } from "resend";
@@ -72,75 +72,66 @@ export default component$(() => {
   return (
     <>
       <div class="py-8 lg:px-36 lg:py-24 xl:px-80">
-        {/* <ContainerAnimated client:visible> */}
-        <p class="h4">Reach out and let's chat.</p>
-        <h1 class="mt-3 lg:mt-7">
-          Got an idea? <span class="gradient">tell me about it.</span>
-        </h1>
-        {/* </ContainerAnimated> */}
-
-        <button
-          onClick$={async () => {
-            const greeting = await serverGreeter("gabriele", "napoli");
-            alert(greeting);
-          }}
-        >
-          greet
-        </button>
+        <ContainerAnimated client:visible>
+          <p class="h4">Reach out and let's chat.</p>
+          <h1 class="mt-3 lg:mt-7">
+            Got an idea? <span class="gradient">tell me about it.</span>
+          </h1>
+        </ContainerAnimated>
 
         <div class="my-16">
-          {/* <ContainerAnimated client:visible> */}
-          <Form class="flex flex-col" onSubmit$={handleSubmit}>
-            <div class="flex gap-3 my-3">
-              <div class="flex-1">
-                <Field name="name">
+          <ContainerAnimated client:visible>
+            <Form class="flex flex-col" onSubmit$={handleSubmit}>
+              <div class="flex gap-3 my-3">
+                <div class="flex-1">
+                  <Field name="name">
+                    {(field, props) => (
+                      <input
+                        {...props}
+                        type="text"
+                        value={field.value}
+                        class="w-full"
+                        placeholder="Name"
+                      />
+                    )}
+                  </Field>
+                </div>
+                <div class="flex-1">
+                  <Field name="email">
+                    {(field, props) => (
+                      <input
+                        {...props}
+                        type="email"
+                        value={field.value}
+                        class="w-full"
+                        placeholder="Email"
+                      />
+                    )}
+                  </Field>
+                </div>
+              </div>
+
+              <div class="my-3 w-full">
+                <Field name="message">
                   {(field, props) => (
-                    <input
+                    <textarea
                       {...props}
-                      type="text"
                       value={field.value}
-                      class="w-full"
-                      placeholder="Name"
+                      class="h-32 w-full"
+                      placeholder="Message"
                     />
                   )}
                 </Field>
               </div>
-              <div class="flex-1">
-                <Field name="email">
-                  {(field, props) => (
-                    <input
-                      {...props}
-                      type="email"
-                      value={field.value}
-                      class="w-full"
-                      placeholder="Email"
-                    />
-                  )}
-                </Field>
-              </div>
-            </div>
 
-            <div class="my-3 w-full">
-              <Field name="message">
-                {(field, props) => (
-                  <textarea
-                    {...props}
-                    value={field.value}
-                    class="h-32 w-full"
-                    placeholder="Message"
-                  />
-                )}
-              </Field>
-            </div>
-
-            <button
-              type="submit"
-              class="rounded-lg bg-white text-black py-3 font-semibold mt-3"
-            >
-              Send message
-            </button>
-          </Form>
-          {/* </ContainerAnimated> */}
+              <button
+                type="submit"
+                class="rounded-lg bg-white text-black py-3 font-semibold mt-3"
+              >
+                Send message
+              </button>
+            </Form>
+          </ContainerAnimated>
         </div>
       </div>
     </>
